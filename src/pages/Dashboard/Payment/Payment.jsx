ded52e7041e3131e542e8954ad1e2fe05823a335
muhammad samim reza <muhammadsamimreza@gmail.com>
2025-12-08 17:14:54 +0600

    Payment intigration done

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -14,11 +14,25 @@ const Payment = () => {
      return result.data;
     },
   });
+  console.log(book)
+  const handlePayment = async ()=>{
+    const paymentInfo ={
+        price: book.price,
+        bookTitle: book.bookTitle,
+        bookId: book.bookId,
+        userEmail: book.userEmail
+    }
+    const res = await axiosSecure.post('/payment-checkout-session', paymentInfo);
+    console.log(res.data)
+    window.location.href = res.data.url;
+
+  }
    if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Failed to load</p>;
   return (
-    <div>
-      <h1>paymet {book.price} for {book.bookTitle}</h1>
+    <div className="space-y-2">
+      <h1 className="text-2xl font-mono">Paymet {book.price} for {book.bookTitle}</h1>
+      <button onClick={handlePayment} className="btn btn-success">Pay</button>
     </div>
   );
 };
